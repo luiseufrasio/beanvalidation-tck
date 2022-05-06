e06713c3cc1e1076f5a9dbc727e00cc4418bf359
luiseufrasio <luis.eufrasio@gmail.com>
2022-05-06 08:34:00 -0300

    tck_version

--- setup-examples/artifact-install.sh
+++ setup-examples/artifact-install.sh
@@ -6,15 +6,15 @@ TCK_DIST=/mnt/c/Users/luise/git/tck/beanvalidation-tck-dist-3.0.0
 # Parent pom
 mvn org.apache.maven.plugins:maven-install-plugin:3.0.0-M1:install-file \
 -Dfile=${TCK_DIST}/src/pom.xml -DgroupId=jakarta.validation \
--DartifactId=beanvalidation-tck-parent -Dversion=${tck.version} -Dpackaging=pom
+-DartifactId=beanvalidation-tck-parent -Dversion=3.0.0 -Dpackaging=pom
 
 # Install the tests jar
 mvn org.apache.maven.plugins:maven-install-plugin:3.0.0-M1:install-file \
--Dfile=${TCK_DIST}/artifacts/beanvalidation-tck-tests-${tck.version}.jar -DgroupId=jakarta.validation \
--DartifactId=beanvalidation-tck-tests -Dversion=${tck.version} -Dpackaging=jar
+-Dfile=${TCK_DIST}/artifacts/beanvalidation-tck-tests-3.0.0.jar -DgroupId=jakarta.validation \
+-DartifactId=beanvalidation-tck-tests -Dversion=3.0.0 -Dpackaging=jar
 
 # Install the test-suite.xml
 mvn org.apache.maven.plugins:maven-install-plugin:3.0.0-M1:install-file \
 -Dfile=${TCK_DIST}/artifacts/tck-tests.xml -DgroupId=jakarta.validation \
--DartifactId=beanvalidation-tck-tests -Dversion=${tck.version} -Dpackaging=xml -Dclassifier=suite
+-DartifactId=beanvalidation-tck-tests -Dversion=3.0.0 -Dpackaging=xml -Dclassifier=suite
 
